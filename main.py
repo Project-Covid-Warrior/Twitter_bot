d73b03874beae96d85a8e5d32979c3c0e9796746
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-05-13 13:52:27 +0530

    added function to store and retrieve ids

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ consumer_secret = os.getenv("CONSUMER_SECRET")
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(token, token_secret)
 api = tweepy.API(auth)
+
+def retrieve_id(file):
+    ids = []
+    with open(file, 'r') as f:
+        for i in range(len(f)):
+            if '\n' in f[i]:
+                ids.append(f[i][:-1].strip())
+            else:
+                ids.append(f[i].strip())
+    return ids
+
+def store_id(id, file):
+    with open(file, 'a') as f:
+        f.write(str(id) + '\n')
+    return
+
